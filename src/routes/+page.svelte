187d2c23712d20b6ab14377b27ba1ec53f3ac9dc
That1GuyWhoSucks <79329354+That1GuyWhoSucks@users.noreply.github.com>
2025-03-27 08:16:27 -0400

    SPA fix?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 import { goto } from "$app/navigation";
 function ViewData() {
-    goto("/view");
+    goto("#/view");
 }
 function CreateConfig() {
-    goto("/create");
+    goto("#/create");
 }
+
 </script>
 <h3>Welceom</h3>
 <button onclick="{CreateConfig}">Create config for testing</button>
