7f5179d7587bad347fdaba40289a686bc9ba9c32
That1GuyWhoSucks <79329354+That1GuyWhoSucks@users.noreply.github.com>
2025-03-27 01:16:40 -0400

    fix??

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-		
-	}
+export default {
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+    }),
+    paths: {
+      base: '/That1GuyWhoSucks.github.io', // Set to match GitHub Pages repo name
+    },
+    prerender: {
+      entries: ['*'],
+    },
+  }
 };
-
-export default config;
